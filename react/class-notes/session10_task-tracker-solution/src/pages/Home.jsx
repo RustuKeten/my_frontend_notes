b98e981d85f2f9140ab394d356fe0fa087e097fa
RustuKeten <ketenrustu@gmail.com>
2022-07-05 07:48:06 +0300

    taskTracker

--- react/class-notes/session10_task-tracker-solution/src/pages/Home.jsx
+++ react/class-notes/session10_task-tracker-solution/src/pages/Home.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import AddTaskForm from "../components/AddTaskForm";
 import Header from "../components/Header";
-import { useState } from "react";
-import data from "../helper//starterData";
+import ShowTasks from "../components/ShowTasks";
+import { useState, useEffect } from "react";
+// import data from '../helper/starterData';
 
 const Home = () => {
-  const [tasks, setTasks] = useState();
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  console.log(tasks);
   return (
     <div>
-      <Header tasks={tasks} />
-      <AddTaskForm />
+      <Header tasks={tasks} setTasks={setTasks} />
+      <ShowTasks />
     </div>
   );
 };
--- react/projects/recap10_task-tracker-solution - Kopya/src/components/Header.jsx
+++ react/projects/recap10_task-tracker-solution - Kopya/src/components/Header.jsx
@@ -1,23 +1,19 @@
 import AddTaskForm from "./AddTaskForm";
 import { useState } from "react";
 
-const Header = (tasks) => {
+const Header = () => {
   const [show, setShow] = useState(false);
-
   const [btnStyle, setBtnStyle] = useState({
     name: "SHOW ADD TASK BAR",
     bgColor: "purple",
   });
-
-  //? 📢 React doesn't update states instantly, it updates them in bulk. States are updated with async logic to avoid over-rendering
   //! React, default olarak state'leri hemen degistirmeyebilir.
   //! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
   //! Bir event handler icerisindeki ardasik state'ler event bitiminde toplu bir
-  //! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gorere yapilir.
+  //! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gore yapilir.
   //! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
 
   //? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
-
   const handleShow = () => {
     if (show) {
       setBtnStyle({
@@ -30,9 +26,10 @@ const Header = (tasks) => {
         bgColor: "red",
       });
     }
+
     setShow(!show);
   };
-
+  // console.log(show);
   return (
     <header className="header">
       <h1>TASK TRACKER</h1>
